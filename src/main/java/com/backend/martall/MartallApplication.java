ced2be29152e9067a07fd34903c629e2323a5a33
rjswjddn <rjswjddn@gmail.com>
2024-01-21 22:17:49 +0900

    first commit

---
+++ src/main/java/com/backend/martall/MartallApplication.java
@@ -0,0 +1,13 @@
+package com.backend.martall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MartallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/martall/MartallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.martall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MartallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
