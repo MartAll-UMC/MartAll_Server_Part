46f5fa555a8d0e769ce269803c98ff73308c7ec6
방지민 <bjimin7@naver.com>
2024-01-22 00:16:10 +0900

    [Chore]: BaseTime Setting

--- src/main/java/com/backend/martall/MartallApplication.java
+++ src/main/java/com/backend/martall/MartallApplication.java
@@ -2,12 +2,12 @@ package com.backend.martall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MartallApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MartallApplication.class, args);
 	}
-
 }
