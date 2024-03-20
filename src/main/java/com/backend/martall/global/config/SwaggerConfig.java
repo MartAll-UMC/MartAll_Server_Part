889f20b95d6e7e8aa4731898c956884008f7cc6a
rjswjddn <rjswjddn@gmail.com>
2024-03-20 22:53:40 +0900

    [Feat] Swagger jwt 인증 추가

--- src/main/java/com/backend/martall/global/config/SwaggerConfig.java
+++ src/main/java/com/backend/martall/global/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.backend.martall.global.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +12,20 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
+        // SecuritySecheme명
+        String jwtSchemeName = "access-token";
+        // API 요청헤더에 인증정보 포함
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        // SecuritySchemes 등록
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.APIKEY)
+                        .in(SecurityScheme.In.HEADER));
+
+
         return new OpenAPI()
-                .components(new Components());
+                .addSecurityItem(securityRequirement)
+                .components(components);
     }
 }
