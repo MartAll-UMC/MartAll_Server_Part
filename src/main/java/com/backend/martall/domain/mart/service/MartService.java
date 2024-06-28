07b8b01f37245ae53c5532136b4cd322b316da5e
rjswjddn <rjswjddn@gmail.com>
2024-06-28 15:39:45 +0900

    [Fix]: 오늘의 마트 DTO 수정

--- src/main/java/com/backend/martall/domain/mart/service/MartService.java
+++ src/main/java/com/backend/martall/domain/mart/service/MartService.java
@@ -205,20 +205,18 @@ public class MartService {
 
     public List<MartRecommendedResponseDto> getTodayMart() {
         List<MartShop> martShopList = martRepository.findRandomMart(PageRequest.of(0, 8));
-        List<MartRecommendedResponseDto> martRecommendedResponseDtoList = martShopList.stream()
+        return martShopList.stream()
                 .map(martShop -> {
-                    MartRecommendedResponseDto martRecommendedResponseDto = MartRecommendedResponseDto.builder()
+                    return MartRecommendedResponseDto.builder()
                             .martId(martShop.getMartShopId())
                             .martImg(martShop.getProfilePhoto())
                             .martName(martShop.getName())
-                            .martCategory(martShop.getMartCategories().stream()
+                            .category(martShop.getMartCategories().stream()
                                     .map(martCategory -> martCategory.getCategoryName())
                                     .collect(Collectors.toList()))
                             .build();
-                    return martRecommendedResponseDto;
                 })
                 .collect(Collectors.toList());
-        return martRecommendedResponseDtoList;
     }
 
     public List<String> recommendMartKeyword() {
