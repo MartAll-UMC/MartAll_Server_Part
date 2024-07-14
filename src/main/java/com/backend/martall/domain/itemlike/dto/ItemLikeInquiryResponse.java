20bebddf7d90fbe3f97c0ba127cdd5cffd50e22a
rjswjddn <rjswjddn@gmail.com>
2024-07-14 14:16:13 +0900

    [Fix]: Swagger의 response와 dto와 다른 이유 해결

--- src/main/java/com/backend/martall/domain/itemlike/dto/ItemLikeInquiryResponse.java
+++ src/main/java/com/backend/martall/domain/itemlike/dto/ItemLikeInquiryResponse.java
@@ -1,6 +1,5 @@
 package com.backend.martall.domain.itemlike.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -9,15 +8,10 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ItemLikeInquiryResponse {
-    @Schema(example = "1")
     private int itemId;
-    @Schema(example = "상품 이미지")
     private String itemImg;
-    @Schema(example = "황금 바나나")
     private String itemName;
-    @Schema(example = "77777")
     private int itemPrice;
-    @Schema(example = "true")
     private boolean itemLike;
     private Mart mart;
 
@@ -27,9 +21,7 @@ public class ItemLikeInquiryResponse {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class Mart {
-        @Schema(example = "1")
         private Long martId;
-        @Schema(example = "바나나올")
         private String martName;
     }
 
